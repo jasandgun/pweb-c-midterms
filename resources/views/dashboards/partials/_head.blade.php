cabf7e80ab91ffcea7a58905b60aca541fc9f21b
jasandgun <jasandgun@gmail.com>
2021-10-29 20:04:44 +0700

    fix: case-sensitive css

--- resources/views/dashboards/partials/_head.blade.php
+++ resources/views/dashboards/partials/_head.blade.php
@@ -1,16 +1,16 @@
-<meta charset="utf-8') }}">
-<meta http-equiv="X-UA-Compatible" content="IE=edge') }}">
-<meta name="viewport" content="width=device-width, initial-scale=1') }}">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>@yield('title')</title>
 
- <!-- Theme style -->
- <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
- <!-- Google Font: Source Sans Pro -->
- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
- <!-- Font Awesome -->
- <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
- 
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ asset('dist/css/AdminLTE.min.css') }}">
+<!-- Google Font: Source Sans Pro -->
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+
 
 
 
